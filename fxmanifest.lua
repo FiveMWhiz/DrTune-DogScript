29586c7599bdf6b237ce05ba75035a89b67cbfea
Devlar <a1.studio.9099@gmail.com>
2022-11-05 12:56:28 +0200

    devlar-woof

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games {'gta5' }
+
+client_scripts {
+    '@NativeUI/NativeUI.lua',
+    'client.lua'
+}
+
+dependencies {
+    'NativeUI'
+}
+
+name 'devlar-woof'
+description 'The dog goes woof.'
+author 'Devlar-Solutions'
